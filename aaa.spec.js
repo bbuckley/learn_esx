28394cc1f9ebd9b4c185adad67c5245b86519f10
Brian Buckley <briankbuckley@gmail.com>
2017-03-10 12:34:09 -0500

    add two spec files

---
+++ aaa.spec.js
@@ -0,0 +1,15 @@
+
+// import { filters } from './filters'
+
+describe('aaa', () => {
+
+  it('should handle initial state', () => {
+
+
+  })
+
+  it('should handle invalid method', () => {
+
+  })
+
+})
---
+++ aaa2.spec.js
@@ -0,0 +1,15 @@
+
+// import { filters } from './filters'
+
+describe('aaa2', () => {
+
+  it('should handle initial state', () => {
+
+
+  })
+
+  it('should handle invalid method', () => {
+
+  })
+
+})
