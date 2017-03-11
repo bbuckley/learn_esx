8076455324c44ae38a78e42287303cb2daf05937
Brian Buckley <briankbuckley@gmail.com>
2017-03-11 08:37:36 -0500

    edit isArray.js

--- isArray.js
+++ isArray.js
@@ -3,9 +3,11 @@ const a = {}
 
 a.b = [1,2,3,4,5]
 
-//console.log(a.b)
-//console.log(a.c)
+const d = 0
+
 console.log(typeof a.c === 'undefined')
 console.log(typeof a.b !== 'undefined')
+console.log(typeof(a.b) !== 'undefined')
+console.log((typeof d === 'undefined') === false)
 console.log(Array.isArray(a.b) === true)
 console.log(Array.isArray(a.c) === false)
