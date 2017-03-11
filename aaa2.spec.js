118b493b8e921c90c62d27dd0af1d46988fd7b8a
Brian Buckley <briankbuckley@gmail.com>
2017-03-11 12:18:25 -0500

    miscellaneous

--- aaa2.spec.js
+++ aaa2.spec.js
@@ -2,10 +2,9 @@
 // import { filters } from './filters'
 
 describe('aaa2', () => {
-
   it('should handle initial state', () => {
 
-
+    const x  = 0;``
   })
 
   it('should handle invalid method', () => {
