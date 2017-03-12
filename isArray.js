aebb7026530bfae2fcfca00fd82f928f9ffd5c5a
Brian Buckley <briankbuckley@gmail.com>
2017-03-12 10:11:21 -0400

    misc

--- isArray.js
+++ isArray.js
@@ -11,3 +11,8 @@ console.log(typeof(a.b) !== 'undefined')
 console.log((typeof d === 'undefined') === false)
 console.log(Array.isArray(a.b) === true)
 console.log(Array.isArray(a.c) === false)
+
+
+export const tt = () => console.log('printing ttt')
+export const ss = () => console.log('printing sss')
+//export default uu = () => console.log('printing uu')
--- isArray.spec.js
+++ isArray.spec.js
@@ -1,14 +1,17 @@
 
 
+import { ss, tt } from './isArray'
+
 describe('array', () => {
 
-  it('should handle initial state', () => {
-
+  it('should know Array.length', () => {
+    expect([1,2].length).toEqual(2)
+    expect([].length).toEqual(0)
 
   })
 
   it('should handle invalid method', () => {
-
+    tt()
   })
 
 })
