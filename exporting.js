41bf0e42daa3201c11b1249f834b5f1deb3c7c57
Brian Buckley <briankbuckley@gmail.com>
2017-03-17 13:46:32 -0400

    cleanup

---
+++ exporting.js
@@ -0,0 +1,10 @@
+
+
+const x = () => console.log('printing x')
+const y = () => console.log('printing y')
+const z = () => console.log('printing z')
+const a = () => "aaa"
+
+module.exports = {
+  x, y, z, a
+}
---
+++ spec/aaa.spec.js
@@ -0,0 +1,21 @@
+
+// import { filters } from './filters'
+
+describe('aaa', () => {
+
+  it('should handle initial state', () => {
+    //expect(1).toEqual(2)
+    expect(1).toEqual(1)
+    expect(true).toBe(true);
+
+    //expect(1)
+
+
+
+  })
+
+  it('should handle invalid method', () => {
+
+  })
+
+})
