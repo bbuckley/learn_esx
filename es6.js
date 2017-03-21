fe5305e6f933d583f9598f09a838319c17a50a29
Brian Buckley <briankbuckley@gmail.com>
2017-03-20 20:07:05 -0400

    add set.js

---
+++ es6.js
@@ -0,0 +1,18 @@
+
+const g = x => 2 * x
+const h = x => 15 * x
+const j = x => x + 100
+const i = x => h(g(j(x)))
+
+//console.log([11,1 , 3, 5 ,10].map(x => i(x)));
+
+
+const a = {
+  b: 7777,
+  c: 8888,
+  d: 9999,
+}
+
+const { b } = a
+const [ aa, bb, {cc} ] = [555, 777, { cc: 888}]
+console.log(b, aa, cc)
